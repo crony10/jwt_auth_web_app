26e2aa967478d52fd999b1aac7679ac8f3f7abe1
Crony <cronypaul@gmail.com>
2021-09-22 13:24:55 +0530

    completed jwt

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,41 @@
+import React,{Fragment,useState,useEffect} from "react";
+import { toast } from "react-toastify";
+
+
+const Dashboard = ({setAuth})=>{
+
+    const [name,setName] = useState("");
+
+    async function getName(){
+        try {
+            const response = await fetch("http://localhost:5000/dashboard/",{
+                method: "GET",
+                headers: {token: localStorage.token}
+            });
+            const parseRes = await response.json();
+            
+            setName(parseRes.user_name);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    useEffect(()=>{
+        getName()
+    },[]);
+
+    const logout = (e)=>{
+        e.preventDefault();
+        localStorage.removeItem("token");
+        setAuth(false);
+        toast.success("Logged out successully!")
+    } 
+    return(
+        <Fragment>
+            <h1>dashboard {name}</h1>
+            <button className="btn btn-primary" onClick={e=>logout(e)}>Logout</button>
+        </Fragment>
+    )
+}
+
+export default Dashboard;
